e44a3281c53d726342c54774cda78c414f3112a8
Mac Tavish <49161243+Mac000@users.noreply.github.com>
2022-06-17 19:27:23 +0500

    Added "formatted_name" to Product which contains product name as per Company
    This attribute is meant to be used for display to user. for application logic, "name" will be used.

--- database/migrations/2022_02_07_124613_create_products_table.php
+++ database/migrations/2022_02_07_124613_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('formatted_name');
             $table->json('data');
             $table->decimal('price')->nullable();
             $table->string('currency')->nullable();
