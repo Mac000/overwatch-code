3cba0ce94041a1bb127bb94c697de3d1b7916be2
Mac Tavish <49161243+Mac000@users.noreply.github.com>
2022-08-13 19:55:02 +0500

    Added  Telescope, Nova & Blog to Header

--- resources/views/components/common/header.blade.php
+++ resources/views/components/common/header.blade.php
@@ -44,6 +44,26 @@
                             Vehicles
                         </a>
                     </li>
+                    <li>
+                        <a class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                           href="{{App::currentLocale() === 'ur' ? route('ur_blog'): route('blog')}}">
+                            Blog
+                        </a>
+                    </li>
+                    @env('local')
+                        <li>
+                            <a class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                               href="{{config('app.url')}}/telescope">
+                                Telescope
+                            </a>
+                        </li>
+                        <li>
+                            <a class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                               href="{{config('app.url')}}/nova">
+                                Nova
+                            </a>
+                        </li>
+                    @endenv
                 </ul>
             </div>
         </div>
